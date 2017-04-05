b9ccc1dbcfd37744e2be33962f3a99baa99943f7
vetu11 <vetu@hotmail.es>
2017-04-06 00:31:02 +0200

    v0.6.2

--- cosas/__init__.py
+++ cosas/__init__.py
@@ -1,5 +1,5 @@
 from .objetos import Partida, Usuario
 from .TOKEN import TOKEN
-from .funciones import nuevosMensajes, enviarMensaje, error, debbugPrint
+from .funciones import nuevosMensajes, enviarMensaje, error
 from .newMessage import NewMessage
 from .revisarMensajes import Revisar
